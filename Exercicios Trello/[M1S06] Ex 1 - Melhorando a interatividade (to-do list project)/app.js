cfb06236a4aa7ffb902001f5faa1971457d14c16
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2023-11-10 19:43:09 -0300

    Exercicios Trello: Módulo 1 - Semana 6

---
+++ Exercicios Trello/[M1S06] Ex 1 - Melhorando a interatividade (to-do list project)/app.js
@@ -0,0 +1,96 @@
+const taskInput = document.getElementById("taskInput");
+let tasks = [];
+
+function add() {
+  let task = taskInput.value;
+  console.log("Add: "+task);
+
+  tasks.push(task);
+  
+  var index = tasks.indexOf(task);
+  console.log("index: "+index);
+
+  if(task== ""){
+    alert("Preencha o campo:");
+    return;
+  }
+
+  update();
+}
+
+function remove(){
+  let task = taskInput.value;
+  console.log("Remove: "+task);
+
+  var index = tasks.indexOf(task);
+  console.log("index: "+index);
+
+  tasks = tasks.splice(index, 1);
+  update();
+}
+
+function clear() {
+  document.getElementById("taskInput").value = "";
+}
+
+function update(){
+  let task = taskInput.value;
+
+  tasks.forEach((t) => 
+  newTask.innerHTML = 
+    `
+      <td class="border"><input type="checkbox" class="checkbox" id="check1" name="check4" value="check" /></td>
+      <td class="border"><label class="labelCheck" id="check4Label" for="check4Label">E${task}</label></td>
+      <td><input type="image" class="removeBtn" id="remove4Btn" src="images/trash.png" title="Excluir" alt="Submit"></td>
+    `
+  );
+
+  console.log("list: " + tasks);
+  clear();
+}
+
+//var primeiro = frutas.shift(); // remove Maçã do início
+//var ultimo = frutas.pop(); // remove Laranja (do final)
+//var adicionar = frutas.unshift("Morango"); // adiciona ao início
+const addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function() {
+  add();
+});
+
+const check1 = document.getElementById("check1");
+const check2 = document.getElementById("check2");
+const check3 = document.getElementById("check3");
+const check4 = document.getElementById("check4");
+
+check1.addEventListener("change", function() {
+  if (check1.checked) {
+    document.getElementById("check1Label").style.textDecoration = "line-through";
+  }else {
+    document.getElementById("check1Label").style.textDecoration = "none";
+  }
+});
+
+check2.addEventListener("change", function() {
+  if (check2.checked) {
+    document.getElementById("check2Label").style.textDecoration = "line-through";
+  }else {
+    document.getElementById("check2Label").style.textDecoration = "none";
+  }
+});
+
+check3.addEventListener("change", function() {
+  if (check3.checked) {
+    document.getElementById("check3Label").style.textDecoration = "line-through";
+  }else {
+    document.getElementById("check3Label").style.textDecoration = "none";
+  }
+});
+
+check4.addEventListener("change", function() {
+  if (check4.checked) {
+    document.getElementById("check4Label").style.textDecoration = "line-through";
+  }else {
+    document.getElementById("check4Label").style.textDecoration = "none";
+  }
+});
+
