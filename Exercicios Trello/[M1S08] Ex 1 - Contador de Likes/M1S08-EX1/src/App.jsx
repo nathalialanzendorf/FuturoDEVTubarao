4a10786f6f422f3985ef87e83f4bf3ce8eada2a1
Nathalia Lanzendorf <nathalia.lanzendorf@gmail.com>
2023-12-18 21:49:38 -0300

    [M1S08] Ex 1 - Contador de Likes

--- Exercicios Trello/[M1S08] Ex 1 - Contador de Likes/M1S08-EX1/src/App.jsx
+++ Exercicios Trello/[M1S08] Ex 1 - Contador de Likes/M1S08-EX1/src/App.jsx
@@ -1,32 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import faviconLogo from './assets/favicon.ico'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Header } from './components/HeaderComponent'
+import { Title } from './components/TitleComponent'
+import { Body } from './components/BodyComponent'
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={faviconLogo} className="logo favicon" alt="Favicon logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-      </div>
+      <Header />
+      <Title />
+      <Body />
     </>
   )
 }
